1668164c8986ca7e4f121adfe72fc38e8355b5ab
Jeferson_Vasconez <Jhvasconez@espe.edu.ec>
2026-01-16 10:26:00 -0500

    V 1.0

---
+++ Documents/Proyectos net/Ejercicio2/src/ejercicio2/AumentoNotas.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author labesp
+ */
+public interface AumentoNotas {
+    void aplicar(double[] notas);
+    
+}
